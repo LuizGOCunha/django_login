fc99b28c4ac3df3196355318112b6851a287cd43
Luiz <valuiz1@gmail.com>
2023-01-11 17:01:56 -0300

    Initial commit

---
+++ login_page/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+# Remember to import views
+
+urlpatterns = [
+    path('signin/', views.signin, name='signin'),
+    path('signup/', views.signup, name='signup'),
+    path('', views.home, name='home')
+]
---
+++ login_page/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def signin(request):
+    pass
+
+def signup(request):
+    pass
+
+def home(request):
+    pass
+
