c6228722ec854a60a1572e210af64e552a691e8a
Luiz <valuiz1@gmail.com>
2023-01-18 13:52:46 -0300

    Created some tests. Need to expand on it when i have more time.

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 import pytest
 
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import check_password
 
 
 def test_if_we_can_create_a_user(return_test_user):
@@ -9,14 +10,16 @@ def test_if_we_can_create_a_user(return_test_user):
     count = User.objects.count()
     assert count == 1
 
+def test_if_created_user_has_correct_fields(return_test_user):
+    user = return_test_user
+    assert user.username == "usernametest"
+    assert user.email == "test@test.com"
+    assert user.check_password('passwordtest') is True
+
 @pytest.mark.django_db
 # The above decorator can be used to create a db inside a test
-# however, i already created a fixture that creates a db for me,
-# so we are using that instead
 def test_if_we_can_access_other_tests_db():
     count = User.objects.count()
     assert count != 1
 
-def test_if_we_can_check_users_password(return_test_user):
-    user = return_test_user
-    assert user.check_password('passwordtest') is True
+
